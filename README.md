e87f416263a72654a19f153948e9a2d3bc38e40c
Thales Oliveira <98770961+undefined@users.noreply.github.com>
2022-05-10 12:33:33 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Web-Dev---Thales
-Created with WebComponents.dev
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+  nome = "Thales do Bronx";
+  telefone = "23 98313153";
+  cpf = "365.313.585-22";
+  idade = 33;
+  email = "thalesmgn@gmail.com";
+  instagram = "@tsmagno";
+  linkedIn = "https://www.linkedin.com/in/thales-oliveira-3494b8218/";
+}
